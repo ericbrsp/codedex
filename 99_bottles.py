c7c1f7b3e30aa82b280bfb499a87bd0fd92cf3bd
Eric Gonçalves <eric.cadastro@gmail.com>
2025-06-20 23:22:56 -0300

    Create 99_bottles.py

---
+++ 99_bottles.py
@@ -0,0 +1,5 @@
+for i in range(99, 0, -1):
+  print(f'{i} botles of beer on the wall')
+  print(f'{i} botles of beer')
+  print('Take one down, pass it around')
+  print(f'{i - 1} botles of beer on the wall')

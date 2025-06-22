abd209eb85ca69adbc42beac48db51b8349ee5d3
Eric Gonçalves <eric.cadastro@gmail.com>
2025-06-22 00:19:16 -0300

    Create playlist.py

---
+++ playlist.py
@@ -0,0 +1,12 @@
+playlist = [
+  'Porches - rangerover',
+  'Mount Eerie - You Swan, Go On',
+  'Carolyn Polachek - Look at Me Now',
+  'Pinegrove - Darkness',
+  'LVL UP - Spirit Was',
+  'Mitski - First Love / Late Spring'
+]
+
+
+for song in playlist:
+  print(song)

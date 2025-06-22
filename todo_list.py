51d4459f1a6627b9456e647ea854fbef6ad819c7
Eric Gonçalves <eric.cadastro@gmail.com>
2025-06-21 23:50:08 -0300

    Create todo_list.py

---
+++ todo_list.py
@@ -0,0 +1,13 @@
+todo = ['get_quarters', 
+'do_laundry', 
+'take_a_walk', 
+'get_a hair_cut', 
+'make_some_tea', 
+'complete_lists_chapter', 
+'call_mom', 
+'watch_my_hero_academia']
+
+print(todo[0])
+print(todo[1])
+print(todo[2 : 5])
+print(todo[9])

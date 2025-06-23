665c93bd9b6fae1c5d6ee4b138b87c20eae5a770
Eric Gonçalves <eric.cadastro@gmail.com>
2025-06-22 22:41:07 -0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def add(a, b):
+  add_res = a + b
+  return add_res
+
+def subtract(a, b):
+  sub_res = a - b
+  return sub_res
+
+def multiply(a, b):
+  mult_res = a * b
+  return mult_res
+
+def divide(a, b):
+  div_res = a / b
+  return div_res
+
+def exp(a, b):
+  exp_res = a ** b
+  return exp_res

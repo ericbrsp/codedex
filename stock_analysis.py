1d6015657fe0557855a4d6a6c2a2bc13bed9552c
Eric Gonçalves <eric.cadastro@gmail.com>
2025-06-22 23:29:23 -0300

    Create stock_analysis.py

---
+++ stock_analysis.py
@@ -0,0 +1,23 @@
+stock_prices = [34.68, 36.09, 34.94, 33.97, 34.68, 35.82, 43.41, 44.29, 44.65, 53.56, 49.85, 48.71, 48.71, 49.94, 48.53, 47.03, 46.59, 48.62, 44.21, 47.21]
+
+
+def price_at(x):
+  return stock_prices[x-1]
+
+def max_price(a, b):
+  values = stock_prices[a-1 : b]
+  return max(values)
+
+def min_price(a, b):
+  values = stock_prices[a-1 : b]
+  return min(values)
+
+#valor no dia x
+print(price_at(1))
+
+#valor máximo da lista entre os dias a e b
+print(max_price(1, 3))
+
+#valor mínimo da lista entre os dias a e b
+print(min_price(1, 3))
+

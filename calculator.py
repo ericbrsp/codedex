4d7a4afce0f08fd1453c2009509da7fcbd74ab4a
Eric Gonçalves <eric.cadastro@gmail.com>
2025-06-22 22:42:11 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -17,3 +17,30 @@ def divide(a, b):
 def exp(a, b):
   exp_res = a ** b
   return exp_res
+
+
+
+#mais simples:
+
+'''
+def add(a, b):
+  return a + b
+
+def subtract(a, b):
+  return a - b
+
+def multiply(a, b):
+  return a * b
+
+def divide(a, b):
+  return a / b
+
+def exp(a, b):
+  return a ** b
+
+print(add(3, 5))
+print(subtract(7, 2))
+print(multiply(4, 8))
+print(divide(9, 3))
+print(exp(2, 3))
+'''

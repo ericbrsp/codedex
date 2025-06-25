2490729bf72c2fbc0ae6c252d25f6289d95a3649
Eric Gonçalves <eric.cadastro@gmail.com>
2025-06-25 17:12:03 -0300

    Create datetime.py

---
+++ datetime.py
@@ -0,0 +1,8 @@
+
+import datetime
+
+print("Data completa:", datetime.datetime.now())
+print("Data completa:", datetime.date.today())  
+print("Apenas o ano:", datetime.date.today().day)    
+print("Apenas o ano:", datetime.date.today().month)    
+print("Apenas o ano:", datetime.date.today().year)

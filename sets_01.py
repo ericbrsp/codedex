ad89319a95da18b8d9f3feaccf8853686a17b486
Eric Gonçalves <eric.cadastro@gmail.com>
2025-06-30 23:25:34 -0300

    Create sets_01.py

---
+++ sets_01.py
@@ -0,0 +1,10 @@
+
+my_list = {'abacate', 'manga', 'kiwi', 'maracuja'}
+friend_list = {'banana', 'mamao', 'maracuja'}
+
+union_set = my_list.union(friend_list)
+intersection = my_list.intersection(friend_list)
+
+print(union_set)
+print(intersection)
+

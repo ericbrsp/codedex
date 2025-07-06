7e72337ef00b252924728bf87671cbcb2acf3a34
Eric Gonçalves <eric.cadastro@gmail.com>
2025-07-06 14:55:14 -0300

    Create higher_function.py

---
+++ higher_function.py
@@ -0,0 +1,27 @@
+
+# Define the Higher-order function 
+def apply_operation(operation, numbers):
+  result = []
+  for num in numbers:
+    result.append(operation(num))
+  return result
+
+# Example operation
+def double(x):
+  return x * 2
+
+# List of numbers
+numbers_list = [1, 2, 3, 4, 5]
+
+# Using the higher-order function
+doubled_numbers = apply_operation(double, numbers_list)
+
+# Displaying the outcomes
+print('Original Numbers:', numbers_list)
+print('Doubled Numbers:', doubled_numbers)
+
+
+
+#apply_operation() is our higher-order function.
+#A function takes another function (double()) as an argument and applies it to each item in numbers_list.
+#The result is a fast and customizable way to transform the numbers based on the chosen operation.

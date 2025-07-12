f5efafe646fc132c024c0e8a8fb395519be2a18f
Eric Gonçalves <eric.cadastro@gmail.com>
2025-07-11 23:28:26 -0300

    Add files via upload

---
+++ unit-tests/string_units.py
@@ -0,0 +1,14 @@
+
+
+def reverse_string(s):
+  return s[::-1] # técnica de fatiamento que inverte a string
+
+def capitalize_string(s):
+  return s.capitalize() # retorna uma string com a primeira letra maiuscula e o restante em minúsculo
+
+def is_capitalized(s):
+   return s[0].isupper() # pega o primeiro caractere e verifica se está em maiúscula
+
+
+
+
---
+++ unit-tests/test_strings_utils.py
@@ -0,0 +1,22 @@
+
+import unittest
+from string_units import reverse_string, capitalize_string, is_capitalized
+
+
+class TestStringUtils(unittest.TestCase):
+
+    def test_reverse_string(self):
+        self.assertEqual(reverse_string('mochi'), 'ihcom')
+
+    def test_capitalize_string(self):
+        self.assertEqual(capitalize_string('mochi'), 'Mochi')
+
+    def test_is_capitalized(self):
+        self.assertTrue(is_capitalized('Mochi'))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
